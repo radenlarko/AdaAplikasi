cc1a9f96064f022b70ae971fda49ae74a7b00266
radenlarko <radenlarko@gmail.com>
2021-01-20 17:01:08 +0700

    styling halaman register

---
+++ Code/mern-blog/src/components/atoms/index.js
@@ -0,0 +1,5 @@
+import Input from './Input';
+import Button from './Button';
+import Gap from './Gap';
+
+export {Input, Button, Gap};
--- Code/mern-blog/src/pages/Register/index.js
+++ Code/mern-blog/src/pages/Register/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { RegisterBg } from '../../assets';
+import { Button, Gap, Input } from '../../components';
 import './register.scss'
 
 const Register = () => {
     return (
         <div className="main-page">
             <div className="left">
-                <p>Ilustrasi</p>
+                <img src={RegisterBg} className="bg-image" alt="register-bg" />
             </div>
             <div className="right">
-                <p>Form Register</p>
+                <p className="title">Register</p>
+                <Input label="Full Name" placeholder="Full Name" />
+                <Input label="Email" placeholder="Email" />
+                <Input label="Password" placeholder="Password" />
+                <Gap height={20} />
+                <Button title="Register" />
             </div>
         </div>
     )
