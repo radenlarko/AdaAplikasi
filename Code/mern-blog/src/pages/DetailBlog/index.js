ac8f9fc4f713021d3f5125bf77d97e04fdcde5b2
radenlarko <radenlarko@gmail.com>
2021-01-22 20:20:51 +0700

    Membuat halaman Detail Blog

--- Code/mern-blog/src/pages/DetailBlog/index.js
+++ Code/mern-blog/src/pages/DetailBlog/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import {RegisterBg} from '../../assets'
+import './detailBlog.scss'
 
 const DetailBlog = () => {
     return (
-        <div>
-            <p>Content detail blog</p>
+        <div className="detail-blog-wrapper">
+            <img className="img-cover" src={RegisterBg} alt="thumb" />
+            <p className="blog-title">Title Blog</p>
+            <p className="blog-author">Author - Date Post</p>
+            <p className="blog-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem rem suscipit totam aut quibusdam, nihil veritatis odio a perferendis corrupti quas? Laudantium ratione ea quod, consequatur laborum inventore magni deserunt?</p>
         </div>
     )
 }
