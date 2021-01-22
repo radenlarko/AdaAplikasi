7d026728dca9d0e8bc4f086157e0c853c66ea00a
radenlarko <radenlarko@gmail.com>
2021-01-22 20:03:27 +0700

    membuat halaman create blog

--- Code/mern-blog/src/pages/CreateBlog/index.js
+++ Code/mern-blog/src/pages/CreateBlog/index.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Button, Gap, Input, TextArea, Upload } from '../../components'
+import './createBlog.scss'
 
 const CreateBlog = () => {
     return (
         <div>
-            <p>Content create blog</p>
+            <div className="blog-post">
+                <p className="title">Create new blog post</p>
+                <Input label="Post Title" placeHolder="Post Title" />
+                <Upload />
+                <TextArea />
+                <Gap height={20} />
+                <div className="button-action">
+                    <Button title="Post" />
+                </div>
+            </div>
+            <Gap height={40} />
         </div>
     )
 }
