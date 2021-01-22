81b6a52d00c07f76ba13d350578751ab327bc7d4
radenlarko <radenlarko@gmail.com>
2021-01-22 14:46:37 +0700

    Styling Main App

--- Code/mern-blog/src/assets/index.js
+++ Code/mern-blog/src/assets/index.js
@@ -1,4 +1,11 @@
 import RegisterBg from './images/register-bg.jpg';
 import LoginBg from './images/login-bg.jpg';
+import AdaAplikasi from './images/AdaAplikasi.svg';
 
-export {RegisterBg, LoginBg};
+// Icon
+import ICFacebook from './icons/facebook.svg';
+import ICInstagram from './icons/instagram.svg';
+import ICTwitter from './icons/twitter.svg';
+import ICGithub from './icons/github.svg';
+
+export {RegisterBg, LoginBg, AdaAplikasi, ICFacebook, ICInstagram, ICTwitter, ICGithub};
--- Code/mern-blog/src/pages/MainApp/index.js
+++ Code/mern-blog/src/pages/MainApp/index.js
@@ -1,25 +1,29 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { CreateBlog, DetailBlog, Home } from '..'
+import { Footer, Header } from '../../components'
+import './mainApp.scss'
 
 const MainApp = () => {
     return (
-        <div>
-            <p>Header</p>
-            <Router>
-                <Switch>
-                    <Route path="/create-blog">
-                        <CreateBlog />
-                    </Route>
-                    <Route path="/detail-blog">
-                        <DetailBlog />
-                    </Route>
-                    <Route path="/">
-                        <Home />
-                    </Route>
-                </Switch>
-            </Router>
-            <p>Footer</p>
+        <div className="main-app-wrapper">
+            <Header />
+            <div className="content-wrapper">
+                <Router>
+                    <Switch>
+                        <Route path="/create-blog">
+                            <CreateBlog />
+                        </Route>
+                        <Route path="/detail-blog">
+                            <DetailBlog />
+                        </Route>
+                        <Route path="/">
+                            <Home />
+                        </Route>
+                    </Switch>
+                </Router>
+            </div>
+            <Footer />
         </div>
     )
 }
