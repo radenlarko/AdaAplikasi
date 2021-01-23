a7058575bedc98fc56b41f5930a87ee47cc421f4
radenlarko <radenlarko@gmail.com>
2021-01-23 16:43:58 +0700

    Membuat Validasi request body Memberikan Error response yang dinamis

--- Code/mern-api/src/controllers/blog-create.js
+++ Code/mern-api/src/controllers/blog-create.js
@@ -1,7 +1,17 @@
+const {validationResult} = require('express-validator');
+
 exports.blogCreate = (req, res, next) => {
     const title = req.body.title;
     // const image = req.body.image;
     const body = req.body.body;
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        const err = new Error('Input Value tidak sesuai');
+        err.errorStatus = 400;
+        err.data = errors.array();
+        throw err;
+    }
 
     const result = {
         message: 'Create Blog Post Success!',
--- Code/mern-api/src/routes/blog-create.js
+++ Code/mern-api/src/routes/blog-create.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const {body} = require('express-validator');
 
 const router = express.Router();
 
 const blogCreateController = require('../controllers/blog-create');
 
-router.post('/post', blogCreateController.blogCreate);
+router.post('/post', [
+    body('title').isLength({min: 5}).withMessage('Input Title tidak sesuai'), 
+    body('body').isLength({min: 5}).withMessage('Isi Artikel tidak sesuai')], 
+    blogCreateController.blogCreate);
 
 module.exports = router;
