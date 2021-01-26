94c258db0c7ca0418805fbd96d85b8e3ac51d007
radenlarko <radenlarko@gmail.com>
2021-01-26 23:31:34 +0700

    Implementasi Pagination di Homepage

--- Code/mern-blog/src/config/redux/action/homeAction.js
+++ Code/mern-blog/src/config/redux/action/homeAction.js
@@ -1,11 +1,18 @@
 import axios from "axios";
 
-export const setDataBlog = () => (dispatch) => {
-    axios.get('http://localhost:4000/v1/blog/posts?page=2&perPage=3')
+export const setDataBlog = (page) => (dispatch) => {
+    axios.get(`http://localhost:4000/v1/blog/posts?page=${page}&perPage=3`)
     .then(result => {
         // console.log('data API', result.data);
         const responseAPI = result.data;
         dispatch({type: 'UPDATE_DATA_BLOG', payload: responseAPI.data});
+        dispatch({
+            type: 'UPDATE_PAGE', 
+            payload: {
+                currentPage: responseAPI.current_page, 
+                totalPage: Math.ceil(responseAPI.total_data / responseAPI.per_page)
+            }
+        });
     })
     .catch(err => {
         console.log('error: ', err);
