cb2d2fc002eed1bb3b794d664ae4c1475f3753c7
radenlarko <radenlarko@gmail.com>
2021-01-27 22:53:50 +0700

    implementasi menambahkan post baru dan upload image

--- Code/mern-blog/src/pages/CreateBlog/index.js
+++ Code/mern-blog/src/pages/CreateBlog/index.js
@@ -1,21 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Gap, Input, Link, TextArea, Upload } from '../../components'
 import './createBlog.scss'
 import {useHistory} from 'react-router-dom';
+import axios from 'axios';
 
 const CreateBlog = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [image, setImage] = useState('');
+    const [imagePreview, setImagePreview] = useState(null);
     const history = useHistory();
+
+    const onSubmit = () => {
+        console.log('title: ', title);
+        console.log('body :', body);
+        console.log('image :', image);
+
+        const data = new FormData();
+        data.append('title', title);
+        data.append('body', body);
+        data.append('image', image);
+
+        axios.post('http://localhost:4000/v1/blog/post', data, {
+            headers: {
+                'content-type': 'multipart/form-data'
+            }
+        })
+        .then(res => {
+            console.log('Post Success :', res);
+        })
+        .catch(err => {
+            console.log('Error :', err);
+        })
+    }
+
+    const onImageUpload = (e) => {
+        const file = e.target.files[0];
+        setImage(file);
+        setImagePreview(URL.createObjectURL(file));
+    }
     return (
         <div>
             <div className="blog-post">
                 <Link title="kembali" onClick={() => history.push('/')} />
                 <p className="title">Create new blog post</p>
-                <Input label="Post Title" placeHolder="Post Title" />
-                <Upload />
-                <TextArea />
+                <Input label="Post Title" placeholder="Post Title" value={title} onChange={(e) => setTitle(e.target.value)} />
+                <Upload onChange={(e) => onImageUpload(e)} img={imagePreview} />
+                <TextArea value={body} onChange={(e) => setBody(e.target.value)} />
                 <Gap height={20} />
                 <div className="button-action">
-                    <Button title="Post" />
+                    <Button title="Post" onClick={onSubmit} />
                 </div>
             </div>
             <Gap height={40} />
